dad230b93be5150be8af5c20df800d8d61e0da94
Andre-Tobois <tobois.andre@gmail.com>
2018-12-13 22:23:26 -0500

    feat: Added Material-ui to project, created the first page for step form.

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class FormUserDetails extends Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div><h1>wassup party ppl</h1></div>
+    );
+  }
+}
+
+export default FormUserDetails;
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,6 +1,7 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
 
-class UserForm extends Component { // eslint-disable-line react/prefer-stateless-function
+ export class UserForm extends Component { // eslint-disable-line react/prefer-stateless-function
   state = {
     step : 1,
     firstName : '',
@@ -9,17 +10,52 @@ class UserForm extends Component { // eslint-disable-line react/prefer-stateless
     occupation : '',
     city : '',
     bio : ''
+  };
+
+  // Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    })
   }
+
+  // Go back to prev step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    })
+  }
+
+  // Handle fields change
+  handleChange = (input) => e => {
+    this.setState({[input]: e.target.value
+  })}
 
   render() {
-    return (
+    const { step } = this.state;
+    const { firstName, lastName, email, occupaction, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupaction, city, bio }
 
-    );
+    switch(step){
+      case 1:
+      return(
+        <FormUserDetails
+          nextStep = {this.nextStep}
+          handleChange = {this.handleChange}
+          values = {values}
+        />
+      )
+      case 2:
+        return <h1>FormPersonalDetails</h1>
+      case 3:
+        return <h1>Confirm</h1>
+      case 4:
+        return <h1>Success</h1>
+    }
   }
 }
 
-ComponentName.propTypes = {
-  prop: PropTypes.type.isRequired
-}
 
 export default UserForm;
