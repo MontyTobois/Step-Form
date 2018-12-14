42ecf92ef54056ce9bc8c5b3c1c7d448cd6431c2
Andre-Tobois <tobois.andre@gmail.com>
2018-12-13 22:38:55 -0500

    feat: created a header with hamburger menu.

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import  MuiThemeProvider  from 'material-ui/styles/MuiThemeProvider';
+import  AppBar  from 'material-ui/AppBar';
+import  TextField  from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export class FormUserDetails extends Component { // eslint-disable-line react/prefer-stateless-function
+  continue = e => () => {
+    e.preventDefaut();
+    this.props.nextStep();
+  }
+
   render() {
+    const { values } = this.props;
     return (
-      <div><h1>wassup party ppl</h1></div>
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title = "Enter you Info" />
+        </React.Fragment>
+      </MuiThemeProvider>
     );
   }
 }
