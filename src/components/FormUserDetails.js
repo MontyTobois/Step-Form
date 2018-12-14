914384b270093fccb2addb79cf48688349cc3625
Andre-Tobois <tobois.andre@gmail.com>
2018-12-14 11:19:26 -0500

    feat: User is able to enter info and click to second page of form.

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -5,20 +5,53 @@ import  TextField  from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
 export class FormUserDetails extends Component { // eslint-disable-line react/prefer-stateless-function
-  continue = e => () => {
-    e.preventDefaut();
+  continue = e => {
+    e.preventDefault();
     this.props.nextStep();
-  }
+  };
 
   render() {
-    const { values } = this.props;
+    const { values, handleChange } = this.props;
     return (
       <MuiThemeProvider>
         <React.Fragment>
-          <AppBar title = "Enter you Info" />
+          <AppBar title = "Enter User Information" />
+          <TextField
+            hintText="Enter your First Name"
+            floatingLabaelText= "First Name"
+            onChange={handleChange('firstName')}
+            defaultValue={values.firstName}
+          />
+          <br/>
+          <TextField
+            hintText="Enter your Last Name"
+            floatingLabaelText= "Last Name"
+            onChange={handleChange('lastName')}
+            defaultValue={values.lastName}
+          />
+          <br/>
+          <TextField
+            hintText="Enter your Email"
+            floatingLabaelText= "Email"
+            onChange={handleChange('email')}
+            defaultValue={values.email}
+          />
+          <br/>
+          <RaisedButton
+            label="Continue"
+            primay={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
         </React.Fragment>
       </MuiThemeProvider>
     );
+  }
+}
+
+const styles = {
+  button:{
+    margin:15
   }
 }
 
